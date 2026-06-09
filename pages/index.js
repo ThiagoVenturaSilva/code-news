9ecfed096526a2e3f7dee3a93ba03e99b091b72b
ThiagoVenturaSilva <159443183+ThiagoVenturaSilva@users.noreply.github.com>
2026-06-09 13:19:44 +0000

    Comitt index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home (){
 
-   return  <h1> Olá, você está na pagina home do code-news, o começo da nossa jornada</h1>
+   return  <h1> Olá, você está na pagina home do code-news</h1>
 
 }
 
